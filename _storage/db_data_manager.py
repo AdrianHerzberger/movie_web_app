003f6869bdf28fc596ca255958d431f30f997784
Adrian Herzberger <adrian@Adrians-Air.fritz.box>
2024-08-26 10:24:39 +0200

    project setup

---
+++ _storage/db_data_manager.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+from .data_manager_interface import DataManagerInterface
+
+class SQLiteDataManager(DataManagerInterface):
+    def __init__(self, db_file_name):
+        self.db = SQLAlchemy(db_file_name)
+        
+        
+        
+    def get_all_users(self):
+        pass
