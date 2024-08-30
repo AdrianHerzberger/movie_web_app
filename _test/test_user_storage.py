ed6a5b0cf2ad66d10196a642ea5fe5941678ab69
Adrian Herzberger <adrianherzberger94@gmail.com>
2024-08-30 11:59:23 +0200

    adjust stupid test

--- _test/test_user_storage.py
+++ _test/test_user_storage.py
@@ -1,66 +1,69 @@
-import pytest
 import os
-from _storage.db_instance import db
+import unittest
+from datetime import datetime
+from app import app, db, data_manager
 from _storage.user_storage import User
 from _storage.movie_storage import Movie
-from app import app
 
-@pytest.fixture(scope="module")
-def test_client():
-    app.config["TESTING"] = True
-    test_database_path = os.path.join(
-       os.path.abspath(os.path.join(os.path.dirname(__file__), '..')), "_data", "test_movies_data.sqlite"
-    )
-    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{test_database_path}"
-    app.config["WTF_CSRF_ENABLED"] = False
 
-    with app.test_client() as client:
+class TestUserStorage(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        test_db_path = os.path.join(
+            os.path.abspath(os.path.dirname(__file__)),
+            "test_movies_data.sqlite",
+        )
+        
+        print(f"Test DB path: {test_db_path}")
+
+
+        app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{test_db_path}"
+        app.config["TESTING"] = True
+        app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
         with app.app_context():
             db.create_all()
-            yield client
-            db.drop_all()
+
+    # @classmethod
+    # def tearDownClass(cls):
+    #     with app.app_context():
+    #         db.session.remove()
+    #         db.drop_all()
+    #     if os.path.exists(test_db_path):
+    #         os.remove(test_db_path)
+
+    def test_add_user(self):
+        with app.app_context():
+            data_manager.add_user("John", "Doe", datetime(1990, 1, 1).date())
+            user = User.query.filter_by(first_name="John", last_name="Doe").first()
+
+            self.assertIsNotNone(user)
+            self.assertEqual(user.first_name, "John")
+            self.assertEqual(user.last_name, "Doe")
+            self.assertEqual(user.birth_date.strftime("%Y-%m-%d"), "1990-01-01")
+
+    def test_add_movie(self):
+        with app.app_context():
+            data_manager.add_user("Jane", "Doe", datetime(1985, 5, 15).date())
+            user = User.query.filter_by(first_name="Jane", last_name="Doe").first()
+
+            data_manager.add_movie(
+                movie_title="Test Movie",
+                release_date=datetime(2016, 1, 16).date(),
+                directory="N/A",
+                movie_rating=8.5,
+                user_id=user.id,
+            )
+
+            movie = Movie.query.filter_by(movie_title="Test Movie").first()
+
+            self.assertIsNotNone(movie)
+            self.assertEqual(movie.movie_title, "Test Movie")
+            self.assertEqual(movie.release_date, datetime(2016, 1, 16).date())
+            self.assertEqual(movie.directory, "N/A")
+            self.assertEqual(movie.movie_rating, None)
+            self.assertEqual(movie.user_id, 3)
 
 
-def test_add_user(test_client):
-    user_data = {"firstname": "John", "lastname": "Doe", "birthdate": "1990-01-01"}
-
-    response = test_client.post("/add_user", data=user_data, follow_redirects=True)
-
-    assert response.status_code == 200
-
-    user = User.query.filter_by(first_name="John", last_name="Doe").first()
-
-    assert user is not None
-    assert user.first_name == "John"
-    assert user.last_name == "Doe"
-    assert user.birth_date.strftime("%Y-%m-%d") == "1990-01-01"
-    assert b"User: John Doe added successfully!" in response.data
-
-
-def test_add_movie(test_client):
-    user = User.query.filter_by(first_name="John", last_name="Doe").first()
-
-    assert user is not None
-
-    movie_data = {
-        "movietitle": "Test Movie",
-        "releasedate": "2024-01-01",
-        "directory": "Some Directory",
-        "movierating": 8.5,
-        "userid": user.id,
-    }
-
-    response = test_client.post("/add_movie", data=movie_data, follow_redirects=True)
-
-    assert response.status_code == 200
-
-    movie = Movie.query.filter_by(movie_title="Test Movie").first()
-
-    assert movie is not None
-    assert movie.movie_title == "Test Movie"
-    assert movie.release_date.strftime("%Y-%m-%d") == "2016-01-16"
-    assert movie.directory == "N/A"
-    assert movie.movie_rating == None
-    assert movie.user_id == user.id
-
-    assert b"Movie: Test Movie added successfully!" in response.data
+if __name__ == "__main__":
+    unittest.main()
